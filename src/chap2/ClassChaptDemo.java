9d743db6be6fae62bd1e12552fd4f4797130f9d1
TonyGregg <antonygenil@gmail.com>
2019-02-18 06:53:36 -0600

    Added basic classes from chapter 1 Questions answers done

---
+++ src/chap2/ClassChaptDemo.java
@@ -0,0 +1,42 @@
+package chap2;
+
+/**
+ * Created by genil on 2/16/19 at 12 34
+ **/
+public class ClassChaptDemo {
+    public static void main(String[] args) {
+
+        Parent parent = new Parent();
+
+        Child child = new Child();
+
+
+//        child = (Child) parent; // it is going to give runtime exception
+        //better try this
+
+
+        if (parent instanceof Child) {
+            child = (Child) parent;
+        }
+
+
+    }
+}
+
+class Parent {
+
+    protected void doEat() {
+        System.out.println("Eating chorru");
+    };
+
+}
+
+class Child extends Parent {
+
+    @Override
+    public void doEat() {
+        System.out.println("Eating Biryani");
+    }
+}
+
+
